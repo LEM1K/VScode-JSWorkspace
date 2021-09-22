475ee9de9d8fc8166edef635597210340edbed65
LEM1K <76048133+LEM1K@users.noreply.github.com>
2021-09-22 23:02:42 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const player = {
+    name:"nico",
+    sayHello:function(other) {
+        console.log("hello! " + other + " nice to meet you");
+    }
+};
+
+console.log(player.name);
+player.sayHello("lynn");
