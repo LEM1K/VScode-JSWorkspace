f5fd45b833a843be253d40f0cf5756a9a4e4be3d
LEM1K <76048133+LEM1K@users.noreply.github.com>
2021-09-29 00:39:44 +0900

    event learning

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+//hellos.innerText = "got you"
+//console.dir(title);
+//title.style.color="blue";
 
-const player = {
-    name:"nico",
-    sayHello:function(other) {
-        console.log("hello! " + other + " nice to meet you");
-    }
+
+function handleTitleClick() {
+    h1.style.color = "black"; 
+};
+function handleMouseEvent() {
+    h1.style.color = "blue"; 
+    h1.innerText="mouse is here"; 
+};
+function handleMouseleave() {
+    h1.style.color = "red"; 
+    h1.innerText="mouse is gone";
 };
 
-console.log(player.name);
-player.sayHello("lynn");
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier");
+}
+
+//title.addEventListener("click", handleTitleClick);
+//title.addEventListener("mouseenter", handleMouseEvent);
+//title.addEventListener("mouseleave", handleMouseleave);
+
+h1.onclick = handleTitleClick;
+h1.onmouseenter = handleMouseEvent;
+h1.onmouseleave = handleMouseleave;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
