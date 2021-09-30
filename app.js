393b78d915761966e3789dbe471a5f741f2811f5
LEM1K <76048133+LEM1K@users.noreply.github.com>
2021-09-30 23:04:43 +0900

    login logic

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick() {
-    const clicked = "active";
-    h1.classList.toggle(clicked);
-};
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN = "hidden";
+const USERNAME_KEY = "username";
+
+function paintGreeting(username) {
+    greeting.innerText = `HELLO ${username}`;
+    greeting.classList.remove(HIDDEN);
+}
 
 
-h1.addEventListener("click", handleTitleClick);
+function handleLoginSubmit(info) {
+    info.preventDefault();
+    //const username = loginInput.value;
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY , username);
+    loginForm.classList.add(HIDDEN);
+    paintGreeting(username);
+}
+
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN);
+    loginForm.addEventListener("submit", handleLoginSubmit);
+}
+else {
+    loginForm.classList.add(HIDDEN);
+    paintGreeting(savedUsername);
+}
